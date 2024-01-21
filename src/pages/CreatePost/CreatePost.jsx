e7db4479b303c6d114633356015d0fcd2af7f31b
leonardo <leopereirasilva86@gmail.com>
2024-01-21 12:49:49 -0300

    estrutura da pagina CreatePost

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -1,8 +1,79 @@
 import styles from './CreatePost.module.css'
 
+import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import{ useAuthValue } from "../../context/authContext"
+
 const CreatePost = () => {
+  const  [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [body, setBody] = useState("");
+  const [tags, setTags] = useState([]);
+  const [formError, setFormError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+  
+
   return (
-    <h1>CreatePost</h1>
+    <div className={styles.create_post}>
+      <h2>Criar post</h2>
+      <p>Escreva sobre o que quiser e compartilhe o seu conhecimento</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Título:</span>
+          <input 
+            type="text" 
+            name="title" 
+            required 
+            placeholder="Pense em um bom título..." 
+            onChange={(e) => setTitle = e.target.value}
+            value={title}
+          />
+        </label>
+        <label>
+          <span>Url da imagem:</span>
+          <input 
+            type="text" 
+            name="image" 
+            required 
+            placeholder="Insira uma imagem que representa o seu post..." 
+            onChange={(e) => setImage = e.target.value}
+            value={image}
+          />
+        </label>
+        <label>
+          <span>Conteúdo:</span>
+          <textarea 
+            name="body" 
+            required 
+            placeholder="no que está pensando?" 
+            onChange={(e) => setBody = e.target.value}
+            value={body}
+          ></textarea>
+        </label>
+        <label>
+          <span>Tags:</span>
+          <input 
+            type="text" 
+            name="tags" 
+            required 
+            placeholder="tag1, tag2, tag3..." 
+            onChange={(e) => setTags = e.target.value}
+            value={tags}
+          />
+        </label>
+        <button className="btn">Cadastrar</button>
+        {/*{!loading &&<button className="btn">Cadastrar</button>}
+        {loading && (
+          <button className="btn" disabled>
+            Aguarde...
+          </button>
+        )}
+        {error && <p className="error">{error}</p>}*/}
+      </form>
+    </div>
   )
 }
 
